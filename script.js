b4c5064f409b15b9bd1c34e432f4aad1a83bac17
Mangesh Bodke <mangeshbodke2025@gmail.com>
2025-08-27 23:25:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
  function calculateArea() {
             
-            let radius = prompt("Enter the radius of the circle:");
+        let radius = prompt("Enter the radius of the circle:");
             
 	 let a=3.14;
 	 let b=a*(radius)*(radius)
 return b;
 	 
 }
-alert(calculateArea());
+alert(`The area of the circle with radius ${b} is ${calculateArea()} )`;
